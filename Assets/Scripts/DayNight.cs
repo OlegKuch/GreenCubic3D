c17b2c96f62e852108b97f7c1d680f76ab1e5450
Oleg <49838022+OlegKuch@users.noreply.github.com>
2020-08-25 14:43:47 +0300

    daynignt indev

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public float rotateSpeed = 1;
+    Vector3 rotate = new Vector3(1,0,0);
+    void Update()
+    {
+        transform.Rotate(rotate);
+    }
+}
